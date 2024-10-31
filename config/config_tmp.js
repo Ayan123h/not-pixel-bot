10de5d74b31074c1242e7bd4bfa2fe6592b73008
Ayan123h <babaaraf9@outlook.com>
2024-11-01 03:34:01 +0600

    Update config_tmp.js

--- config/config_tmp.js
+++ config/config_tmp.js
@@ -1,6 +1,6 @@
 export class Config {
-  static TELEGRAM_APP_ID = undefined; // YOUR APP ID EX : 324893724923
-  static TELEGRAM_APP_HASH = undefined; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
+  static TELEGRAM_APP_ID = 29877480; // YOUR APP ID EX : 324893724923
+  static TELEGRAM_APP_HASH = 7122a97c2472fce123dadd90d9cb14de; // YOUR APP HASH EX: "aslkfjkalsjflkasf"
   static MODE = 2; // 1 FOR 1 BY 1 RUN & 2 FOR MASS RUN
   static USEAUTOUPGRADE = true; //USE AUTO UPGRADE OR NO
   static REPAINTER = true; //USE INCORRECT COLOR REPAINTER
