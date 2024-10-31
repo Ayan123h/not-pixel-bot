e07abb67a95d55a3ae1a48facd71787ce96fa272
Ayan123h <babaaraf9@outlook.com>
2024-11-01 03:33:29 +0600

    Update proxy_list_tmp.js

--- config/proxy_list_tmp.js
+++ config/proxy_list_tmp.js
@@ -2,9 +2,7 @@
  * 
  * Example : 
  * export const proxyList = [
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
-    "http://proxyUser:proxyPass@proxyHost:proxyPort",
+    "brd-customer-hl_36ceb5c2-zone-datacenter_proxy2:9z4743gh5vte:brd.superproxy.io:22225",
   ];
  */
 
